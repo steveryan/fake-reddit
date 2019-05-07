5e1635aed4501bdc3f79a520467e1c64cf12a86e
steveryan <steveryan6989@gmail.com>
2019-05-07 00:49:16 -0400

    Pulls titles and images from the corresponding subreddit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,19 @@
 class PostsController < ApplicationController
   def index
     @subreddit_name = params[:subreddit_id]
-    binding.pry
+    @images = make_request
+  end
+
+  def make_request
+    title_and_links = []
+    request = RestClient.get "https://api.reddit.com/r/#{@subreddit_name}"
+    parsed_response = JSON.parse(request.body)
+    posts = parsed_response["data"]["children"]
+    posts.each do |json|
+      if json["data"]["url"].include?(".jpg")
+        title_and_links << {title: json["data"]["title"], url: json["data"]["url"]}
+      end
+    end
+    title_and_links
   end
 end
