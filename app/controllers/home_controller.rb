0e7832f860bc93a478d7f69cc9c28d0e0595f102
steveryan <steveryan6989@gmail.com>
2019-05-29 08:17:22 -0400

    Changes the homepage to nudge users to sign up for an account if not logged in.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,7 @@
 class HomeController < ApplicationController
 
   def show
-    if user_signed_in?
-      @current_user = current_user.email
-    else
-      @current_user = "YOU ARE NOT SIGNED IN"
-    end
+    
   end
 
 end
